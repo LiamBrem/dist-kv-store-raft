90528c188b85ab64f17cbccb993a94442fb5d344
liambrem <liam.e.brem@gmail.com>
2026-02-17 11:35:31 -0500

    feat: node states & log entry

--- raft/log.go
+++ raft/log.go
@@ -1,1 +1,9 @@
 package raft
+
+// Represents a single entry in raft log
+
+type LogEntry struct {
+	Term int // which term entry was created in
+	Index int // position in log
+	Command string // e.g. "PUT ..."
+}
--- raft/state.go
+++ raft/state.go
@@ -1,1 +1,17 @@
 package raft
+
+import "time"
+
+type NodeState int 
+
+const (
+	Follower NodeState = iota
+	Candidate NodeState = iota
+	Leader NodeState = iota
+)
+
+const (
+	HeartbeatInterval = 100 * time.Millisecond
+	ElectiontimeoutMin = 150 * time.Millisecond
+	ElectionTimeoutMax = 300 * time.Millisecond
+)
