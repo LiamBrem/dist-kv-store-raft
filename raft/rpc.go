7e7f46287fc023949b74e32c91c3a82fec9d3603
liambrem <liam.e.brem@gmail.com>
2026-02-24 10:26:51 -0500

    feat: rpc structs

--- raft/rpc.go
+++ raft/rpc.go
@@ -1,1 +1,29 @@
 package raft
+
+// RequestVote is sent by candidates during elections
+type RequestVote struct {
+	Term int // current term of candidate
+	CandidateId int // who's requesting vote
+	LastLogIndex int // index of candidate's last log entry
+	LastLogTerm int // term of candidates last log entry
+}
+
+type RequestVoteReply struct {
+	Term int // responder's current term
+	VoteGranted bool // whether vote was given
+}
+
+// AppendEntries is sent by leader for log replication/heartbeats
+type AppendEntriesArgs struct {
+	Term int // leaders current term
+	LeaderId int
+	PrevLogIndex int
+	PrevLogTerm int
+	Entries []LogEntry // empty for heartbeat
+	LeaderCommit int // leader's commit index
+}
+
+type AppendEntriesReply struct {
+	Term int // Responder's current term
+	Success bool // if follower accepted entries
+}
